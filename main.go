899a4e5cbf7f071f0e885821f214036f01042ec1
unknown <michal.r.adamski@student.put.poznan.pl>
2022-07-10 17:04:06 +0200

    Add basic query parameters validation

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import (
 	"net/http"
 )
 
+type RandomMeanQueryParams struct {
+	Requests uint `form:"requests" binding:"required,gte=0,lte=1000"`
+	Length   uint `form:"length" binding:"required,gte=0,lte=10"`
+}
+
 func randomMeanHandler(c *gin.Context) {
-	requests := c.DefaultQuery("requests", "1")
-	length := c.DefaultQuery("length", "5")
-	c.String(http.StatusOK, "Requests: %s , length: %s", requests, length)
+	randomMeanQueryParams := RandomMeanQueryParams{}
+	if err := c.ShouldBindQuery(&randomMeanQueryParams); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest,
+			gin.H{
+				"error":   "QUERY_PARAMS_ERROR",
+				"message": "Invalid query params. Please check your inputs"})
+		return
+	}
+	c.String(http.StatusOK, "Requests Done!")
 }
 
 func main() {
